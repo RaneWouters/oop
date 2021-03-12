4eef64f080a6e44af8fd243928afd395abecaffa
RaneWouters <1540656951@qq.com>
2021-03-12 19:00:57 +0800

    demo

--- ch7/7_2/main.cpp
+++ ch7/7_2/main.cpp
@@ -1,34 +1,16 @@
 #include <iostream>
 #include <vector>
-#include "Complex.h"
+#include <complex>
 #include "Set.h"
 using namespace std;
 
 int main() {
-    //Set<Complex> set_user = {(Complex){1.2, 2}};
-    Set<Complex> set_user;
-    Complex temp(1.5, 2);
-    set_user.insert(temp);
-    cout << set_user << endl;
-    //    cin >> temp;
-    //    while (temp != 0) {
-    //        set_user.insert(temp);
-    //        cin >> temp;
-    //    }
-    //    set_user.clear();
+    vector<complex<double>> temp = {(1.0, 2.0), (2.0, 3.0), (0.5, 1.0)};
+    Set<complex<double>> set_user(temp);
+    cout << real(temp.at(0)) << endl;
+    temp.at(1) = temp.at(2);
+    cout << real(temp.at(1)) << endl;
+    cout << real(temp.at(2)) << endl;
 
-    //    vector<int>::iterator it = set_user.find(2);
-    //    if(it != vector<int>::iterator(0)){
-    //        cout << *it << endl;
-    //    }
-    //
-    //    cout << set_user.number() << endl;
-
-    //     Set<int> a;
-    //     swap(set_user, a);
-    //     vector<int>::iterator it = a.find(2);
-    //     if (it != vector<int>::iterator(0)) {
-    //        cout << *it << endl;
-    //    }
     return 0;
 }
