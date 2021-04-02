7dd1e8589af82c003c04e5f1584457655ab8c929
RaneWouters <1540656951@qq.com>
2021-04-02 14:28:55 +0800

    demo

--- ch8/8_2/Calculater.h
+++ ch8/8_2/Calculater.h
@@ -1,9 +1,27 @@
 #include <bits/c++config.h>
 #include <cstddef>
 #include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
 using namespace std;
 
-string* Init(char* temp) {
+constexpr double PI = 3.14159265358979323846;
+
+int level(const string& temp) {
+    if ((temp == "tan") || (temp == "sqrt") || (temp == "cos") ||
+        (temp == "sin"))
+        return 3;
+    if (temp == "^")
+        return 2;
+    if ((temp == "*") || (temp == "/") || (temp == "%"))
+        return 1;
+    if ((temp == "+") || (temp == "-"))
+        return 0;
+    return -1;
+}
+
+void Init(char* temp) {
     size_t loc = 0;
     size_t count = 0;
     while (*(temp + loc) != '\0') {
@@ -29,6 +47,12 @@ string* Init(char* temp) {
             } else {
                 ++loc;
             }
+        } else if (*(temp + loc) == 'p') {
+            if (*(temp + loc + 1) == 'i') {
+                loc += 2;
+            } else {
+                ++loc;
+            }
         } else {
             if ((*(temp + loc) >= '0') && (*(temp + loc) <= '9')) {
                 while ((*(temp + loc) >= '0') && (*(temp + loc) <= '9')) {
@@ -40,7 +64,7 @@ string* Init(char* temp) {
         }
         ++count;
     }
-    cout << count << endl;
+    cout << "count" << count << endl;
     string* middle = new string[count]();
     loc = 0;
     size_t string_loc = 0;
@@ -81,6 +105,16 @@ string* Init(char* temp) {
                 ++string_loc;
                 ++loc;
             }
+        } else if (*(temp + loc) == 'p') {
+            if (*(temp + loc + 1) == 'i') {
+                *(middle + string_loc) = to_string(PI);
+                ++string_loc;
+                loc += 2;
+            } else {
+                *(middle + string_loc) = *(temp + loc);
+                ++string_loc;
+                ++loc;
+            }
         } else {
             if ((*(temp + loc) >= '0') && (*(temp + loc) <= '9')) {
                 while ((*(temp + loc) >= '0') && (*(temp + loc) <= '9')) {
@@ -95,8 +129,60 @@ string* Init(char* temp) {
             }
         }
     }
-    for (int q = 0; q <= string_loc; ++q) {
+    for (int q = 0; q < string_loc; ++q) {
         cout << middle[q] << endl;
     }
-    return middle;
+    cout << "string_loc" << string_loc << endl;
+
+    queue<string> RP;
+    stack<string> RP_sign;
+    for (int q = 0; q < string_loc; ++q) {
+        cout << q << endl;
+        if (*middle[q].begin() >= '0' && *middle[q].begin() <= '9') {
+            RP.push(middle[q]);
+        } else if (middle[q] == "(") {
+            RP_sign.push(middle[q]);
+        } else if (middle[q] == ")") {
+            while (RP_sign.top() != "(") {
+                RP.push(RP_sign.top());
+                RP_sign.pop();
+                ++q;
+            }
+            RP_sign.pop();
+        } else {
+            if (RP_sign.empty()) {
+                RP_sign.push(middle[q]);
+            } else {
+                if (RP_sign.top() == "(") {
+                    RP_sign.push(middle[q]);
+                } else if (level(middle[q]) > level(RP_sign.top())) {
+                    RP_sign.push(middle[q]);
+                } else if (level(middle[q]) <= level(RP_sign.top())) {
+                    RP.push(RP_sign.top());
+                    RP_sign.pop();
+                    RP_sign.push(middle[q]);
+                }
+            }
+        }
+//        queue<string> temp_RP = RP;
+//        while (!temp_RP.empty()) {
+//            cout << temp_RP.front() << ' ';
+//            temp_RP.pop();
+//        }
+//        cout << endl;
+    }
+    while (!RP_sign.empty()) {
+        if (RP_sign.top() != "(") {
+            RP.push(RP_sign.top());
+        }
+        RP_sign.pop();
+    }
+
+    queue<string> temp1 = RP;
+    while (!temp1.empty()) {
+        cout << temp1.front() << ' ';
+        temp1.pop();
+    }
+    cout << endl;
 }
+
--- ch8/8_2/main.cpp
+++ ch8/8_2/main.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 #include <string>
 #include "Calculater.h"
+#include <queue>
+#include <stack>
 using namespace std;
 
 int main() {
     char temp[100];
-    cin >> temp;
+    temp[0] = '1';
+    temp[1] = '2';
+    temp[2] = '+';
+    temp[3] = '(';
+    temp[4] = 'c';
+    temp[5] = 'o';
+    temp[6] = 's';
+    temp[7] = '(';
+    temp[8] = '2';
+    temp[9] = '*';
+    temp[10] = 'p';
+    temp[11] = 'i';
+    temp[12] = ')';
+    temp[13] = ')';
+
+//    cin >> temp;
     Init(temp);
+
+//    queue<int> temp;
+//    temp.push(1);
+//    temp.push(3);
+//    temp.pop();
+
+//    string* temp = new string[10]();
+//    *temp = "cos";
+//    *(temp + 1) = "tan";
+//    *(temp + 2) = "sqrt";
+//    cout << (*(temp + 1)->begin()) << endl;
+
+//    cout << level(*(temp + 0)) << endl;
+//    cout << level(*(temp + 1)) << endl;
+//    cout << level(*(temp + 2)) << endl;
+
+//    string temp = "cos";
+//    string temp1 = "1234";
+//    cout << temp << endl;
+//    cout << stod(temp1) + 1 << endl;
+
+//    stack<string> temp;
+//    temp.push("123");
+//    cout << temp.top() << endl;
+//    temp.pop();
     return 0;
 }
